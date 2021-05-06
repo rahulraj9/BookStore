cdf31b08173491d317e2545915199b1cd090894f
RahulRaj <jokingboyrahul@gmail.com>
2021-05-06 21:28:24 +0530

    [Rahul] Redux Implementation

--- bookstorefrontend/src/Component/redux/actions/action.js
+++ bookstorefrontend/src/Component/redux/actions/action.js
@@ -1,7 +1,19 @@
-import { ADD_TO_CART } from '../constants/constants'
-export const addToCart = (data) => {
-    return {
-        type: ADD_TO_CART,
-        data: data
+import Service from '../../../Services/bookService'
+const service = new Service()
+export const GET_BOOKS = "GET_BOOKS"
+export const getBooks = () => {
+    return (dispatch) => {
+        return service.getbook.then(result =>
+            dispatch(books(result.data.data))
+        )
     }
-}
+
+};
+
+export const books = (data) => {
+    console.log(data)
+    return {
+        type: "GET_BOOKS",
+        payload: data
+    };
+};
